fc8f3f78d799cf32efdbc92a4200abf91a01cf84
0xwildhare <sschell1979@gmail.com>
2024-12-05 17:46:09 -0800

    add: parse operator_id from args

--- src/bin/aggregator.rs
+++ src/bin/aggregator.rs
@@ -10,17 +10,21 @@ use eigen_services_blsaggregation::bls_agg::{BlsAggregationServiceResponse, BlsA
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let mut signature: G1Affine;
-    let mut public_key: G1Affine;
+    let mut operator_id: String;
     let mut counter = 0;
     for arg in &args {
         counter += 1;
         if arg.contains("Signature:") {
             signature = parse_to_g1_affine(args[counter].clone(), args[counter+1].clone());
-            println!("Signature: {:?}", signature);
+            println!("Signature(yay): {:?}", signature);
         }
-        if arg.contains("PublicKey:") {
-            public_key = parse_to_g1_affine(args[counter].clone(), args[counter+1].clone());
-            println!("PublicKey: {:?}", public_key);
+        if arg.contains("OperatorID:") {
+            operator_id = args[counter]
+            .clone()
+            .chars()
+            .filter(|c| c.is_alphanumeric())
+            .collect::<String>();
+        println!("OperatorID(yay): {:?}", operator_id);
         }
     }
 }
@@ -38,3 +42,4 @@ fn parse_to_g1_affine(x: String, y: String) -> G1Affine {
         .unwrap();
     G1Affine::new(x_big_int.into(), y_big_int.into())
 }
+
