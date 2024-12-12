0c71032b894e5a3a710c94e6993531ea0928c517
0xwildhare <sschell1979@gmail.com>
2024-12-12 12:13:51 -0800

    add: bls_agg_service

--- src/bin/aggregator.rs
+++ src/bin/aggregator.rs
@@ -6,6 +6,30 @@ use ark_bn254::{G1Affine, G2Affine};
 extern crate num;
 use num::bigint::BigUint;
 use eigen_services_blsaggregation::bls_agg::{BlsAggregationServiceResponse, BlsAggregatorService};
+use eigen_client_avsregistry::reader::AvsRegistryChainReader;
+use eigen_logging::get_test_logger;
+use eigen_services_avsregistry::chaincaller::AvsRegistryServiceChainCaller;
+use eigen_services_operatorsinfo::operatorsinfo_inmemory::OperatorInfoServiceInMemory;
+use eigen_types::{
+    avs::TaskIndex,
+    operator::{QuorumNum, QuorumThresholdPercentages},
+};
+use eigen_utils::{
+    get_provider, get_signer,
+    {
+        iblssignaturechecker::{
+            IBLSSignatureChecker::{self, NonSignerStakesAndSignature},
+            BN254::G1Point,
+        },
+        registrycoordinator::{
+            IRegistryCoordinator::OperatorSetParam, IStakeRegistry::StrategyParams,
+            RegistryCoordinator,
+        },
+    },
+};
+use alloy_primitives::{hex, Bytes};
+use alloy_provider::Provider;
+use std::time::Duration;
 
 use axum::{
     extract::State,
@@ -73,4 +97,53 @@ async fn aggregate_sigs(input: String) {
         println!("OperatorID: {:?}", operator_id);
         }
     }
+
+    let registry_coordinator_address = hex!("eCd099fA5048c3738a5544347D8cBc8076E76494").into();
+    let operator_state_retriever_address = hex!("D5D7fB4647cE79740E6e83819EFDf43fa74F8C31").into();
+    let http_endpoint = String::from("http://localhost:8545");
+    let ws_endpoint = String::from("ws://localhost:8545");
+
+    // Create avs clients to interact with contracts deployed on anvil
+    let avs_registry_reader = AvsRegistryChainReader::new(
+        get_test_logger(),
+        registry_coordinator_address,
+        operator_state_retriever_address,
+        http_endpoint.clone(),
+    )
+    .await
+    .unwrap();
+
+    let operators_info = OperatorInfoServiceInMemory::new(
+        get_test_logger(),
+        avs_registry_reader.clone(),
+        ws_endpoint,
+    )
+    .await
+    .unwrap()
+    .0;
+
+    // Create aggregation service
+    let avs_registry_service =
+    AvsRegistryServiceChainCaller::new(avs_registry_reader.clone(), operators_info);
+
+    let bls_agg_service = BlsAggregatorService::new(avs_registry_service);
+
+    let provider = get_provider(http_endpoint.as_str());
+    let current_block_num = provider.get_block_number().await.unwrap();
+    let task_index = 0;
+    let quorum_nums = Bytes::from([0u8]);
+    let quorum_threshold_percentages: QuorumThresholdPercentages = vec![33];
+    let time_to_expiry = Duration::from_secs(1000);
+
+    bls_agg_service
+            .initialize_new_task(
+                task_index,
+                current_block_num as u32,
+                quorum_nums.to_vec(),
+                quorum_threshold_percentages,
+                time_to_expiry,
+            )
+            .await
+            .unwrap();
+
 }
