0646a0dd44eff8b9adb29503fbaef95c550ec15d
0xwildhare <sschell1979@gmail.com>
2025-01-02 07:54:54 -0800

    test: checking verify_message

--- src/bin/aggregator.rs
+++ src/bin/aggregator.rs
@@ -4,13 +4,16 @@
 // once threshold is reached, send the aggregated signature to something onchain (eas maybe?)
 use ark_bn254::g1::G1Affine;
 extern crate num;
-use num::bigint::BigUint;
-use eigen_services_blsaggregation::bls_agg::{BlsAggregationServiceResponse, BlsAggregatorService};
+use alloy_primitives::{address, hex, Address, Bytes, FixedBytes};
+use alloy_provider::Provider;
 use eigen_client_avsregistry::reader::{AvsRegistryChainReader, AvsRegistryReader};
+use eigen_crypto_bls::{BlsG1Point, Signature};
 use eigen_logging::{get_logger, init_logger};
 use eigen_services_avsregistry::{chaincaller::AvsRegistryServiceChainCaller, AvsRegistryService};
-use eigen_services_operatorsinfo::{operatorsinfo_inmemory::OperatorInfoServiceInMemory, operator_info::OperatorInfoService};
-use eigen_crypto_bls::{Signature, BlsG1Point};
+use eigen_services_blsaggregation::bls_agg::{BlsAggregationServiceResponse, BlsAggregatorService};
+use eigen_services_operatorsinfo::{
+    operator_info::OperatorInfoService, operatorsinfo_inmemory::OperatorInfoServiceInMemory,
+};
 use eigen_types::{
     avs::{TaskIndex, TaskResponseDigest},
     operator::{QuorumNum, QuorumThresholdPercentages},
@@ -22,36 +25,33 @@ use eigen_utils::{
             IBLSSignatureChecker::{self, NonSignerStakesAndSignature},
             BN254::G1Point,
         },
+        operatorstateretriever::OperatorStateRetriever,
         registrycoordinator::{
             IRegistryCoordinator::OperatorSetParam, IStakeRegistry::StrategyParams,
             RegistryCoordinator,
         },
-        operatorstateretriever::OperatorStateRetriever
     },
 };
-use alloy_primitives::{hex, Bytes, FixedBytes, address, Address};
-use alloy_provider::Provider;
-use std::time::Duration;
+use num::bigint::BigUint;
 use std::str::FromStr;
+use std::time::Duration;
 
-use axum::{
-    extract::State,
-    routing::post,
-    Json,
-    Router,
-};
+use axum::{extract::State, routing::post, Json, Router};
+use eigen_crypto_bn254::utils::verify_message;
 use eyre::Result;
-use tracing::{info, debug};
-use tokio_util::sync::CancellationToken;
 use tokio::{task, time::sleep};
+use tokio_util::sync::CancellationToken;
+use tracing::{debug, info};
 
 pub fn parse_to_g1_affine(x: &str, y: &str) -> G1Affine {
-    let x_big_int = x.chars()
+    let x_big_int = x
+        .chars()
         .filter(|c| c.is_numeric())
         .collect::<String>()
         .parse::<BigUint>()
         .unwrap();
-    let y_big_int = y.chars()
+    let y_big_int = y
+        .chars()
         .filter(|c| c.is_numeric())
         .collect::<String>()
         .parse::<BigUint>()
@@ -66,17 +66,13 @@ async fn main() {
 
 pub async fn run_aggregator() -> eyre::Result<()> {
     init_logger(eigen_logging::log_level::LogLevel::Info);
-    let app = Router::new()
-        .route("/aggregate", post(aggregate_sigs));
+    let app = Router::new().route("/aggregate", post(aggregate_sigs));
 
     info!("Starting aggregator server on port 5074...");
-    
-    axum::serve(
-        tokio::net::TcpListener::bind("0.0.0.0:5074").await?,
-        app
-    )
-    .await
-    .map_err(|e| eyre::eyre!("Server error: {}", e))?;
+
+    axum::serve(tokio::net::TcpListener::bind("0.0.0.0:5074").await?, app)
+        .await
+        .map_err(|e| eyre::eyre!("Server error: {}", e))?;
 
     Ok(())
 }
@@ -92,29 +88,31 @@ async fn aggregate_sigs(input: String) {
     for word in &words {
         counter += 1;
         if word.contains("Signature:") {
-            signature = parse_to_g1_affine(words[counter], words[counter+1]);
+            signature = parse_to_g1_affine(words[counter], words[counter + 1]);
             println!("Signature: {:?}", signature);
         }
         if word.contains("OperatorID:") {
             let operator_id_string = words[counter]
-            .chars()
-            .filter(|c| c.is_alphanumeric())
-            .collect::<String>();
-        operator_id = operator_id_string.parse::<FixedBytes<32>>().unwrap();
-        println!("OperatorID: {:?}", operator_id);
+                .chars()
+                .filter(|c| c.is_alphanumeric())
+                .collect::<String>();
+            operator_id = operator_id_string.parse::<FixedBytes<32>>().unwrap();
+            println!("OperatorID: {:?}", operator_id);
         }
         if word.contains("CommitmentHash:") {
             let commitment_hash_string = words[counter]
-            .chars()
-            .filter(|c| c.is_alphanumeric())
-            .collect::<String>();
-        commitment_hash = commitment_hash_string.parse::<FixedBytes<32>>().unwrap();
-        println!("CommitmentHash: {:?}", commitment_hash);
+                .chars()
+                .filter(|c| c.is_alphanumeric())
+                .collect::<String>();
+            commitment_hash = commitment_hash_string.parse::<FixedBytes<32>>().unwrap();
+            println!("CommitmentHash: {:?}", commitment_hash);
         }
     }
 
-    let registry_coordinator_address: Address = address!("eCd099fA5048c3738a5544347D8cBc8076E76494").into(); // TODO: get from config
-    let operator_state_retriever_address: Address = address!("D5D7fB4647cE79740E6e83819EFDf43fa74F8C31").into(); // TODO: get from config
+    let registry_coordinator_address: Address =
+        address!("eCd099fA5048c3738a5544347D8cBc8076E76494").into(); // TODO: get from config
+    let operator_state_retriever_address: Address =
+        address!("D5D7fB4647cE79740E6e83819EFDf43fa74F8C31").into(); // TODO: get from config
     let http_endpoint = String::from("http://ethereum:8545"); // TODO: get from .env
     let ws_endpoint = String::from("ws://ethereum:8545"); // TODO: get from .env
 
@@ -128,23 +126,30 @@ async fn aggregate_sigs(input: String) {
     .await
     .unwrap();
 
-    println!("registry_coordinator_address: {:?}", registry_coordinator_address);
-    println!("operator_state_retriever_address: {:?}", operator_state_retriever_address);
+    println!(
+        "registry_coordinator_address: {:?}",
+        registry_coordinator_address
+    );
+    println!(
+        "operator_state_retriever_address: {:?}",
+        operator_state_retriever_address
+    );
 
-    let operators_info = OperatorInfoServiceInMemory::new(
-        get_logger(),
-        avs_registry_reader.clone(),
-        ws_endpoint,
-    )
-    .await
-    .unwrap()
-    .0;
+    let operators_info =
+        OperatorInfoServiceInMemory::new(get_logger(), avs_registry_reader.clone(), ws_endpoint)
+            .await
+            .unwrap()
+            .0;
     println!("operators_info: {:?}", operators_info);
 
     let cancellation_token: CancellationToken = CancellationToken::new();
     let token_clone = cancellation_token.clone();
     let operators_info_clone = operators_info.clone();
-    task::spawn(async move { operators_info_clone.start_service(&token_clone, 21495071, 0).await }); // TODO: what should the block number be?
+    task::spawn(async move {
+        operators_info_clone
+            .start_service(&token_clone, 21495071, 0)
+            .await
+    }); // TODO: what should the block number be?
 
     println!("wainting 2 seconds...");
     sleep(Duration::from_secs(2)).await;
@@ -153,7 +158,7 @@ async fn aggregate_sigs(input: String) {
 
     // Create aggregation service
     let avs_registry_service =
-    AvsRegistryServiceChainCaller::new(avs_registry_reader.clone(), operators_info.clone());
+        AvsRegistryServiceChainCaller::new(avs_registry_reader.clone(), operators_info.clone());
 
     let bls_agg_service = BlsAggregatorService::new(avs_registry_service.clone());
 
@@ -169,7 +174,10 @@ async fn aggregate_sigs(input: String) {
     println!("Task index: {:?}", task_index);
     println!("Current block number: {:?}", current_block_num);
     println!("Quorum nums: {:?}", quorum_nums);
-    println!("Quorum threshold percentages: {:?}", quorum_threshold_percentages);
+    println!(
+        "Quorum threshold percentages: {:?}",
+        quorum_threshold_percentages
+    );
     println!("Time to expiry: {:?}", time_to_expiry);
     bls_agg_service
         .initialize_new_task(
@@ -186,19 +194,27 @@ async fn aggregate_sigs(input: String) {
     // let my_operator_id = FixedBytes::from_str("0x239442a339edec7728e9d122fe7aa5c9a31529476a9c86d3c698f308e2ad9007").unwrap();
     // let my_operator_addr = avs_registry_reader.get_operator_from_id(*my_operator_id).await.unwrap();
     // println!("My operator address: {:?}", my_operator_addr);
-    let operator_addr = avs_registry_reader.get_operator_from_id(*operator_id).await.unwrap();
+    let operator_addr = avs_registry_reader
+        .get_operator_from_id(*operator_id)
+        .await
+        .unwrap();
     println!("Operator address: {:?}", operator_addr);
+    let operator_info = operators_info.get_operator_info(operator_addr).await;
+    println!("Operator info: {:?}", operator_info);
 
-    let operator_info = operators_info
-        .get_operator_info(operator_addr)
-        .await;
-    println!("Operator info: {:?}", operator_info.unwrap());
-
-    // let my_operator_info = operators_info
-    //     .get_operator_info(my_operator_addr)
-    //     .await;
-    // println!("My operator info: {:?}", my_operator_info.unwrap());
-
+    let operator_info = operator_info.unwrap().unwrap();
+    let signature_for_agg = Signature::new(signature);
+    println!("ALL CAPS TO MAKE THIS EASY TO FIND IN THE LOGS!!!**************************************");
+    println!("public key: {:?}", operator_info.g2_pub_key.g2());
+    println!("commitment hash: {:?}", commitment_hash.as_ref() as &[u8]);
+    println!("signature: {:?}", signature);
+    println!("Checking signature...");
+    let verified = verify_message(
+        operator_info.g2_pub_key.g2(),
+        commitment_hash.as_ref() as &[u8],
+        signature,
+    );
+    println!("Signature verified: {:?}", verified);
 
     println!("getting operator state...");
     // get operator state
@@ -207,19 +223,26 @@ async fn aggregate_sigs(input: String) {
         .await
         .unwrap();
     println!("Operator state: {:?}", operator_state);
-    // println!("DO YOU SEE THIS?");
 
-    let signature_for_agg = Signature::new(signature);
+    // let signedDigest = SignedTaskResponseDigest {
+    //     commitment_hash,
+    //     signature_for_agg,
+    //     operator_id,
 
-    println!("verifying signature...");
-    let signature_result = BlsAggregatorService::verify_signature(
-            task_index,
-            signature_for_agg,
-            operator_state,
-        )
-        .await
-        .unwrap();
+    // };
 
+    // println!("verifying signature...");
+    // let signature_result = BlsAggregatorService::verify_signature(
+    //         task_index,
+    //         &signedDigest,
+    //         operator_state,
+    //         signature_verification_channel
+    //     )
+    //     .await
+    //     .unwrap();
+
+    // TODO: HAVE THE AVS NODE PRINT ALL VALUES TO REPRODUCE THE SIGNATURE (PRIVATE KEYS, ETC.)
+    // REPRODUCE THAT SIGNATURE IN THE BLS_AGG TEST, AND SEE IF IT MATCHES.
 
     println!("Processing signature...");
     println!("Task index: {:?}", task_index);
@@ -227,13 +250,7 @@ async fn aggregate_sigs(input: String) {
     println!("Signature: {:?}", signature);
     println!("Operator ID: {:?}", operator_id);
     bls_agg_service
-        .process_new_signature(
-            task_index,
-            commitment_hash,
-            signature_for_agg,
-            operator_id,
-        )
+        .process_new_signature(task_index, commitment_hash, signature_for_agg, operator_id)
         .await
         .unwrap();
-
 }
