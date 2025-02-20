bb4ca7e658ea9b78c14b2da1fc21958f9b22a986
0xwildhare <sschell1979@gmail.com>
2025-02-20 14:02:19 -0800

    fix: more hardecoded values removed

--- run_prover.sh
+++ run_prover.sh
@@ -194,7 +194,7 @@ while true; do
                 
                 if [ -n "$operator_address_list" ]; then
                     # Construct and execute the cast command
-                    cast_command="~/.foundry/bin/cast send 0xeCd099fA5048c3738a5544347D8cBc8076E76494 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \"updateOperatorsForQuorum(address[][],bytes)\" [[${operator_address_list}]] 0x00 --rpc-url http://ethereum:8545"
+                    cast_command="~/.foundry/bin/cast send $REGISTRY_COORDINATOR_ADDRESS --private-key $PRIVATE_KEY \"updateOperatorsForQuorum(address[][],bytes)\" [[${operator_address_list}]] 0x00 --rpc-url $RPC_URL"
                     
                     echo "Executing cast command..."
                     eval "$cast_command"
